8deaa1d398eb134d8ccb74d5caa3a8f554462d9f
Vikas <rmjvv31@gmail.com>
2023-07-27 18:59:49 +0530

    programs

---
+++ exception handling/try and except.py
@@ -0,0 +1,29 @@
+
+try:
+    print(10/0)
+except:
+    print("erro occurred")
+else:
+    print("error not occurred")
+finally:
+    print("finally block is running")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##try:
+##    print("complete")
+##except:
+##    print("erro occurred")
+##else:
+##    print("error not occurred")
